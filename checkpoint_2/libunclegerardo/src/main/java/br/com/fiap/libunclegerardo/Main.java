d7a0ec48458cae76f487d4b904be03ab64d17f07
Matheus Gusmão <104039223+gusmaomath@users.noreply.github.com>
2024-11-11 20:28:12 -0300

    Update Main.java

--- checkpoint_2/libunclegerardo/src/main/java/br/com/fiap/libunclegerardo/Main.java
+++ checkpoint_2/libunclegerardo/src/main/java/br/com/fiap/libunclegerardo/Main.java
@@ -1,3 +1,16 @@
+/* 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Grupo: Milho
+ * Autores: 
+ *          Fabrício Saavedra   -   rm97631@fiap.com.br
+            Guilherme Akio      -   rm98582@fiap.com.br
+            Matheus Motta       -   rm550352@fiap.com.br
+            Matheus Gusmão      -   rm550826@fiap.com.br
+ */
+
 package br.com.fiap.libunclegerardo;
 
 public class Main {
